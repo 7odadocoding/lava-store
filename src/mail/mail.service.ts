3bd4fe63463dea13dee5af7fbe8c796a634efc2c
7odadocoding <mahmoud.alnakeeb.js@gmail.com>
2023-12-22 00:03:50 +0200

    style:remove comments

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,18 +1,13 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 import { IUser } from 'src/user/user.interface';
-// import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class MailService {
-   constructor(
-      private mailerService: MailerService,
-      // private config: ConfigService,
-   ) {}
+   constructor(private mailerService: MailerService) {}
 
    async sendUserConfirmation(user: IUser, sso: string) {
       await this.mailerService.sendMail({
          to: user.email,
-         // from: '"Support Team" <support@example.com>', // override default from
          subject: 'Welcome to NestStore',
          template: './sso.ejs',
          context: {
